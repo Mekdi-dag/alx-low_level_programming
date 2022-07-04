4bf48b2d344fc7d23b9281f47e490767256ead5a
mekdi-dag <dagnewmekdes@gmail.com>
2022-07-04 15:32:30 -0700

    print alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,8 +11,11 @@ int main(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-        for (letter = 'A'; letter <= 'Z'; letter++)  
+
+	for (letter = 'A'; letter <= 'Z'; letter++)  
                 putchar(letter);
-	putchar('\n')
+
+	putchar('\n');
+
 return (0);
 }
