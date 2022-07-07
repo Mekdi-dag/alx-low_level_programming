cccf44cbed90f95312f88da3907dd71826d52746
mekdi-dag <dagnewmekdes@gmail.com>
2022-07-07 10:10:54 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
 * Return: On success 1.
 * On error, -1 is returned. and errno is set appropriately.
 */
-int _putchar (char c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 #include<unistd.h>
@@ -18,7 +18,7 @@ int _putchar (char c)
  *  Return: On success 1.
  *  On error. -1 is returned, and errno is set appropriately.
  */
-int _putchar (char c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }}
