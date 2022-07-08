0ba69c31703458563a88ef9c98c3f99765ccf44d
mekdi-dag <dagnewmekdes@gmail.com>
2022-07-08 08:21:54 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,15 +8,15 @@
  * Return: Always 0.
  *
  */
-int print_sign(int n)
+int print_sign(int c)
 
 {
-	if (n > 0)
+	if (c > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
+	else if (c == 0)
 	{
 		_putchar('0');
 		return (0);
