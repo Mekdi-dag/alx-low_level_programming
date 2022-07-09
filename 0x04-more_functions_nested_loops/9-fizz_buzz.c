2fb3f24565a43761174b8d8ae9d54cf5af9eb096
mekdi-dag <dagnewmekdes@gmail.com>
2022-07-09 15:12:53 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,41 +7,33 @@
 
 int main(void)
 {
-        int p = 100;
-	int i;
+int p = 100;
+int i;
 
-	i = 1;
-	while (i <= p)
-	{
-		if (i % 3 == 0 && i == 0)
-		{
-			printf("FizzBuzz ");
-		}
-
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-
-		else if (i % 3 == 0)
-
-		{
-			if (i < p)
-				printf("Buzz ");
-			else
-				printf("Buzz");
-		}
-
-		else
-		{
-			printf("%i ", i);
-		}
-
-		i++;
-	}
-	printf("\n");
-
-	return (0);
+i = 1;
+while (i <= p)
+{
+if (i % 3 == 0 && i == 0)
+{
+printf("FizzBuzz ");
 }
-
-
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 3 == 0)
+{
+if (i < p)
+printf("Buzz ");
+else
+printf("Buzz");
+}
+else
+{
+printf("%i ", i);
+}
+i++;
+}
+printf("\n");
+return (0);
+}
